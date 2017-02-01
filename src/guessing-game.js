238a7507a5f556e281add36c88378e1ba9f0e415
ivrik <ivrik14@gmail.com>
2017-02-01 23:57:00 +0300

    GuessingGame: Implemented the Game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,19 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    	this.minValue = 0;
+    	this.maxValue = 0;
+    	this.avgValue = 0;
+    }
 
     setRange(min, max) {
-
+    	this.minValue = min;
+    	this.maxValue = max;
     }
 
     guess() {
-
+       	this.avgValue = Math.round((this.minValue + this.maxValue) / 2);
+       	return this.avgValue
     }
 
     lower() {
-
+    	this.maxValue = this.avgValue;
     }
 
     greater() {
+    	this.minValue = this.avgValue;
 
     }
 }
